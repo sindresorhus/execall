9786216182830a268c7059fdab26c5b1734d8eb1
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-31 14:58:24 +0200

    :goat:

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ var execall = require('./');
 
 test(function (t) {
 	var f = execall(/(\d+)/, '$200 and $400')[0];
-	t.assert(f.match = '200');
-	t.assert(f.sub[0] = '200');
-	t.assert(f.index = 1);
+	t.assert(f.match === '200');
+	t.assert(f.sub[0] === '200');
+	t.assert(f.index === 1);
 	t.assert(execall(/\d+/g, '$200 and $400')[1].match === '400');
 	t.assert(execall(/\d+/g, 'unicorn').length === 0);
 	t.end();
